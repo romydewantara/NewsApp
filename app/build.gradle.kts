c2ee22bdf4491386c4f48f7128f4228708c8694b
Romy <dewantararomy@gmail.com>
2025-02-19 09:44:11 +0700

    - add splash screen library to dependency - add vector drawable as splash screen icon - splash screen initialization in MainActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -49,6 +49,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+
+    // SplashScreen Api
+    implementation(libs.androidx.core.splashscreen)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
